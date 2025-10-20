35944338de03bb8afdfdc6780e803abd5905e85c
Antoni Ferkaluk <tyruji@gmail.com>
2025-10-20 18:40:28 +0200

    Updated ArduinoClient class: created a Send method for robot server communication.

--- src/main/java/com/project1/ArduinoClient.java
+++ src/main/java/com/project1/ArduinoClient.java
@@ -1,45 +1,45 @@
-// import java.net.URI;
-// import java.net.http.HttpClient;
-// import java.net.http.HttpRequest;
-// import java.net.http.HttpResponse;
-// import java.time.Duration;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
+public class ArduinoClient {
+  public static final String IP = "http://192.168.1.1";
+  
+  private HttpClient _client;
+  
+  public ArduinoClient() {
+    _client = HttpClient.newBuilder()
+      .connectTimeout(Duration.ofSeconds(5))
+      .build();
+  }
 
-// import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
-
-// public class ArduinoClient extends Application {
-
+  ///
+  ///   This function sends a GET request to the arduino server at the given endpoint.
+  ///   
+  ///   e.g. Send("/forward") -> Sends the request, should move the robot forward.
+  ///   
+  ///   -----------------------------------------------------------------------------
+  /// 
+  ///   Use the ArduinoEndpoints class for the endpoints, don't hardcode them.
+  /// 
+  ///   e.g. Send(ArduinoEndpoints.FORWARD)
+  /// 
+  public HttpResponse<String> Send(String endpoint) throws Exception {
+    String url = IP + endpoint;
     
-//     @Override
-//     public void start(Stage primaryStage) throws Exception {
-//         // Load the FXML file
-//         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Main.fxml"));
-//         Scene scene = new Scene(loader.load());
+    HttpRequest req = HttpRequest.newBuilder()
+      .uri(URI.create(url))
+      .timeout(Duration.ofSeconds(10))
+      .GET()
+      .build();
 
-//         // Set stage properties
-//         primaryStage.setTitle("Robot Controller");
-//         primaryStage.setScene(scene);
-//         primaryStage.show();
-//     }
-//     public static void main(String[] args) throws Exception {
-//         launch(args);
-        
-//         // String url = "http://192.168.1.1/forward";
-
-//         // HttpClient client = HttpClient.newBuilder()
-//         //         .connectTimeout(Duration.ofSeconds(5))
-//         //         .build();
-
-//         // HttpRequest req = HttpRequest.newBuilder()
-//         //         .uri(URI.create(url))
-//         //         .timeout(Duration.ofSeconds(10))
-//         //         .GET()
-//         //         .build();
-
-//         // HttpResponse<String> res = client.send(req, HttpResponse.BodyHandlers.ofString());
-//         // System.out.println("Body: " + res.body());
-//     }
-// }
+    // TODO: Asynchronous queue for calls.
+      
+    HttpResponse<String> res = _client.send(req, HttpResponse.BodyHandlers.ofString());
+    System.out.println("Body: " + res.body());
+    
+    return res;
+  }
+}
