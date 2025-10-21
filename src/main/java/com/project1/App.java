faccdedeb9656e865af012e61263d7a744cb445b
MMarcel02 <marciniak.marc@gmail.com>
2025-10-21 09:28:51 +0200

    basic GUI layout

--- src/main/java/com/project1/App.java
+++ src/main/java/com/project1/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("GUI"), 800, 800);
+        scene = new Scene(loadFXML("GUI"), 800, 500);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/project1/GUIController.java
+++ src/main/java/com/project1/GUIController.java
@@ -1,11 +1,50 @@
 package com.project1;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class GUIController {
+
+    @FXML
+    private Button bigDecrement;
+
+    @FXML
+    private Button bigIncrement;
+
+    @FXML
+    private Button crabWalkLeft;
+
+    @FXML
+    private Button crabWalkRight;
+
+    @FXML
+    private Button downArrow;
+
+    @FXML
+    private Button leftArrow;
+
+    @FXML
+    private Button rightArrow;
+
+    @FXML
+    private Button smallDecrement;
+
+    @FXML
+    private Button smallIncrement;
+
+    @FXML
+    private Label speedLabel;
+
+    @FXML
+    private Slider speedSlider;
+
+    @FXML
+    private Button stopButton;
 
     @FXML
     private Button upArrow;
@@ -17,13 +56,57 @@ public class GUIController {
     private Color x2;
 
     @FXML
-    private Font x3;
+    void bigDecreaseSpeed(ActionEvent event) {
+
+    }
 
     @FXML
-    private Color x4;
+    void bigIncreaseSpeed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crabWalkLeft(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crabWalkRight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moveBackwards(ActionEvent event) {
+
+    }
 
     @FXML
     void moveForward(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rotateLeft(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rotateRight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void smallDecreaseSpeed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void smallIncreaseSpeed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stopSpeed(ActionEvent event) {
 
     }
 
