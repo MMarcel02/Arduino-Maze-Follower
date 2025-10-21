dc2a823451e1701be2981ffb80b792c014aa7551
Ferkaluk, Antoni (Stud. DACS) <a.ferkaluk@student.maastrichtuniversity.nl>
2025-10-21 15:37:25 +0000

    Edit movement.ino - fixed conflicting head

--- Arduino/server/movement.ino
+++ Arduino/server/movement.ino
@@ -1,13 +1,10 @@
 // --- Motor Pin Definitions ---
 // Assign PWM (speed) and DIR (direction) pins for each motor
 const int FL_PWM = 6,  FL_DIR = 5;     // Front Left Motor
-<<<<<<< HEAD
 // test comment
-=======
 const int FR_PWM =9, FR_DIR=10;        // Front Right 
 const int BL_PWM = A4, BL_DIR = A5;    // Back Left
 const int BR_PWM=11, BR_DIR = 12;      // Back Right
->>>>>>> movement
 
 // --- Variables ---
 int motorSpeed = 80;           // Default speed for all motors (range: 0–255)
