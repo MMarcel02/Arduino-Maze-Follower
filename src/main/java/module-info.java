1a719f51c73f3224d7ea34357e2453daf55fa251
Sk Pt <s.peithis@student.maastrichtuniversity.nl>
2025-10-22 12:34:12 +0200

    swing gui with endpoints half don

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.project1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
+    requires java.desktop;
 
     opens com.project1 to javafx.fxml;
     exports com.project1;
