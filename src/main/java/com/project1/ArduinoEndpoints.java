509e8203a8741c7d0aad3add1cc7190d84165269
Panagiotou, Neofytos (Stud. DACS) <n.panagiotou@student.maastrichtuniversity.nl>
2025-10-22 08:45:47 +0000

    added 3 endpoints stop, spot right, backwards

--- src/main/java/com/project1/ArduinoEndpoints.java
+++ src/main/java/com/project1/ArduinoEndpoints.java
@@ -2,7 +2,10 @@ package com.project1;
 
 public final class ArduinoEndpoints {
     public static final String FORWARD = "/forward";
-  
+    public static final String STOP = "/stop";
+    public static final String SPOT_RIGHT = "/spot_right";
+    public static final String BACKWARD = "/backward";
+    
     // etc...
   
 }
