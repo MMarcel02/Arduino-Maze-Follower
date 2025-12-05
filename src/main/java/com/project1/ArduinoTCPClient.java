41bd7461ba4105fcf5f786d694ba9731da41254b
MMarcel02 <marciniak.marc@gmail.com>
2025-12-05 11:42:02 +0100

    Added re-try to TCP server if it doesnt connect initially

--- src/main/java/com/project1/ArduinoTCPClient.java
+++ src/main/java/com/project1/ArduinoTCPClient.java
@@ -13,8 +13,13 @@ public class ArduinoTCPClient {
     private Socket socket;
     private boolean connected = false;
 
+    // Lambda function, currently defined in GUIController initialize
+    // Consumer accepts an input and runs something without producing output
+    // This way we are decoupling our code in here from GUIController, so we can reuse this class
+    // If we need another TCPClient with a different Consumer method
     private Consumer<String> onDataReceived;
 
+    // We pass that function in and set it to Consumer
     public ArduinoTCPClient(Consumer<String> onDataReceived) {
         this.onDataReceived = onDataReceived;
     }
@@ -22,21 +27,51 @@ public class ArduinoTCPClient {
     public void connect() {
         // We run this in a new thread so we dont freeze our GUI while trying to connect
         new Thread(() -> {
-            try {
-                socket = new Socket(IP, PORT);
-                connected = true;
-                
-                BufferedReader dataReceived = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String line;
+            // Since in initalize we only run connect once, we need to keep our connection alive if we ever disconnect
+            // Thats why everything is wrapped in a while(true), so it keeps retrying the connection
+            while(true) {
+                try {
+                    
+                    System.out.println("Attempting TCP connection...");
+                    socket = new Socket(IP, PORT); // Socket is the endpoint on the Arduino we listen to
+                    
+                    // Only gets here if the new Socket(IP, PORT) didn't throw an Exception that would put is into the catch block
+                    connected = true;
+                    System.out.println("TCP Connected!");
 
-                // Basically loops forever until connection breaks
-                while (connected && (line = dataReceived.readLine()) != null) {
-                    if (onDataReceived != null) {
-                        onDataReceived.accept(line);
+                    // .getInputStream() just gets 1010101... from our Arduino
+                    // InputStreamReader translates the 1010101 into chars like 'H' 'E' ..
+                    // BufferedReader accumulates the chars into a String
+                    BufferedReader dataReceived = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String line;
+    
+                    // Basically loops forever until connection breaks
+                    // readLine() method just gives you the String inside a BufferedReader until it sees a \n
+                    // We do the null check to see if the connection has dropped
+                    while (connected && (line = dataReceived.readLine()) != null) {
+                        if (onDataReceived != null) {
+                            onDataReceived.accept(line);
+                        }
                     }
+
+                    // Only get here if the while loop fails -> connection dropped
+                    System.err.println("TCP server disconnected");
+                } catch (Exception e) {
+                    System.err.println("Failed to connect to TCP server: " + e.getMessage());
+                } finally {
+                    // Only get here if we fail to connect, so we reset our connection and clean up
+                    disconnect();
                 }
-            } catch (Exception e) {
-                System.err.println("Failed to connect to TCP server: " + e.getMessage());
+
+                // Only get here if fail to connect, so we wait a second before restarting our while loop that tries to connect
+                // Usually it fails when we start our app too fast after starting the robot
+                // So the robot hasnt had time to set up the TCP server yet
+                try {
+                    System.out.println("Retrying..");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    break;
+                }
             }
         }).start();
     }
