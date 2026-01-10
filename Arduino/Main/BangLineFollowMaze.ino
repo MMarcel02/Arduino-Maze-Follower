2ed6664c08ef76327e7f9d5053b3c6a3158efe8f
Antoni Ferkaluk <tyruji@gmail.com>
2026-01-10 20:02:08 +0100

    Backtracked hte changes

--- Arduino/Main/BangLineFollowMaze.ino
+++ Arduino/Main/BangLineFollowMaze.ino
@@ -8,25 +8,25 @@ void bangLineFollowMaze() {
       stopAllMotors();
       stateStartTime = currentTime;
       mazeState = OBJECT_DETECTED;
-      
+      break;
     } else if (leftDigitalIRReading == 0 && rightDigitalIRReading == 0) {
       setLineFollowingSpeed(motorSpeedOutsideLineFollow);
       moveForward();
-      
     } else if (leftDigitalIRReading == 1 && rightDigitalIRReading == 0) {
       moveBackward();
       stateStartTime = currentTime;
       mazeState = TURNING_LEFT;
-      
+      break;
     } else if (leftDigitalIRReading == 0 && rightDigitalIRReading == 1) {
       moveBackward();
       stateStartTime = currentTime;
       mazeState = TURNING_RIGHT;
-      
+      break;
     } else if (leftDigitalIRReading == 1 && rightDigitalIRReading == 1) {
       stopAllMotors();
       stateStartTime = currentTime;
       mazeState = JUNCTION_FOUND;
+      break;
     }
     break;
 
@@ -37,6 +37,7 @@ void bangLineFollowMaze() {
       if (leftDigitalIRReading == 0 && rightDigitalIRReading == 0) {
         stateStartTime = currentTime;
         mazeState = BLIND_TURN;
+        break;
       }
     }
     break;
@@ -44,6 +45,7 @@ void bangLineFollowMaze() {
   case BLIND_TURN:
     if (currentTime - stateStartTime >= BlindTime) {
       mazeState = FOLLOW_LINE;
+      break;
     }
     break;
 
@@ -54,6 +56,7 @@ void bangLineFollowMaze() {
       if (leftDigitalIRReading == 0 && rightDigitalIRReading == 0) {
         stateStartTime = currentTime;
         mazeState = BLIND_TURN;
+        break;
       }
     }
     break;
@@ -62,23 +65,24 @@ void bangLineFollowMaze() {
     if (currentTime - stateStartTime >= ObjectFoundTime) {
       targetAngleEnd = radToDeg(robotAngle) + 170;
       mazeState = TURNING_180_DEGREES;
+      break;
     }
     break;
 
-  case TURNING_180_DEGREES:
+  case TURNING_180_DEGREES: {
     boolean isTurning = turnToAbsoluteAngle(targetAngleEnd);
     if (!isTurning) {
       stopAllMotors();
       mazeState = AFTER_180_RIGHT_SENSOR_SEARCH;
     }
-    break;
+  } break;
 
   case AFTER_180_RIGHT_SENSOR_SEARCH:
     setLineFollowingSpeed(100);
     turnOnSpotLeft();
-    
     if (rightDigitalIRReading == 1) {
       mazeState = FOLLOW_LINE;
+      break;
     }
     break;
 
@@ -86,34 +90,36 @@ void bangLineFollowMaze() {
     if (currentTime - stateStartTime >= ObjectFoundTime) {
       targetTotalDistance = totalDistance + 0.05;
       mazeState = CLEAR_JUNCTION;
+      break;
     }
     break;
 
-  case CLEAR_JUNCTION:
+  case CLEAR_JUNCTION: {
     boolean isMoving = moveToDistance(targetTotalDistance);
     if (!isMoving) {
       stateStartTime = currentTime;
       mazeState = SEARCHING_FOR_LINE_90_DEG_LEFT;
     }
-    break;
+  } break;
 
   case SEARCHING_FOR_LINE_90_DEG_LEFT:
     if (currentTime - stateStartTime >= ObjectFoundTime) {
       targetAngleEnd = radToDeg(robotAngle) + 110;
       targetAngleStart = radToDeg(robotAngle) - 5;
       mazeState = TURNING_100_DEGREES_LEFT;
+      break;
     }
     break;
 
-  case TURNING_100_DEGREES_LEFT:
+  case TURNING_100_DEGREES_LEFT: {
     boolean isTurning = turnToAbsoluteAngle(targetAngleEnd);
     if (!isTurning) {
       stopAllMotors();
       mazeState = RETURN_RIGHT;
     }
-    break;
+  } break;
 
-  case RETURN_RIGHT:
+  case RETURN_RIGHT: {
     if (currentTime - stateStartTime >= ObjectFoundTime) {
       boolean isTurning = turnToAbsoluteAngle(targetAngleStart);
       if (isTurning) {
@@ -121,11 +127,12 @@ void bangLineFollowMaze() {
           stopAllMotors();
           mazeState = FOLLOW_LINE;
         }
-      } else { // the robot never is lost
+      } else if (!isTurning) { // the robot never is lost
+        stopAllMotors();
         mazeState = LOST_ROBOT;
       }
     }
-    break;
+  } break;
 
   case LOST_ROBOT:
     stopAllMotors();
