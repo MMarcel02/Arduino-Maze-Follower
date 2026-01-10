9f320b42b900fb43f2e043dcdcc89cbfbccdd976
Ferkaluk, Antoni (Stud. DACS) <a.ferkaluk@student.maastrichtuniversity.nl>
2026-01-10 19:08:09 +0000

    Revert "Add obstacleFound helper function"
    This reverts commit 54c7c88694604ee3ef199b38389c77276945d23d

--- Arduino/Main/SensorManager.ino
+++ Arduino/Main/SensorManager.ino
@@ -64,10 +64,3 @@ void updateSensors() {
 String buildSensorMessage() {
   return String(distance, 2) + "," + String(leftDigitalIRReading) + "," + String(rightDigitalIRReading)+ "," + String(leftAnalogIRReading) + "," + String(rightAnalogIRReading);
 }
-
-boolean obstacleFound(){
-  if (0 < distance && distance < emergencyStopDistance) {
-    return true;
-  }
-  return false;
-}
