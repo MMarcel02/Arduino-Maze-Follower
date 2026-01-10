5b0585b185cd48f28eaf9e005b73c0b10b9887d1
Antoni Ferkaluk <tyruji@gmail.com>
2026-01-10 20:42:32 +0100

    Changed max average from 2 times to 5 times

--- Arduino/Main/SensorManager.ino
+++ Arduino/Main/SensorManager.ino
@@ -118,7 +118,7 @@ bool detectEndOfLine() {
     if (bounceFilled >= 5 && avgBounceTime > 0) {
       // we check if the current time without bouncing
       // is more than 2 times the calculated average
-      if ((now - straightStartTime) > (avgBounceTime * 2)) {
+      if ((now - straightStartTime) > (avgBounceTime * 5)) {
         return true;
       }
     }
